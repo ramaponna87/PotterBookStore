ec3044a75f9d5657435bbaa8c67b018efc405bc6
Rama Ponna <rama.ponna@neuven.co.uk>
2024-04-17 16:25:30 +0100

    Added BookRepository implementation with sample book data and GetAllBooks method.

--- Repository/BookRepository.cs
+++ Repository/BookRepository.cs
@@ -9,9 +9,23 @@ namespace PotterBookStore.Repository
 {
     public class BookRepository : IBookRepository
     {
+        private readonly List<Book> _books;
+        public BookRepository()
+        {
+            // Initialize with sample book data
+            _books = new List<Book>
+        {
+            new Book { Id = 1, Title = "Book 1", Price = 8 },
+            new Book { Id = 2, Title = "Book 2", Price = 8 },
+            new Book { Id = 3, Title = "Book 3", Price = 8 },
+            new Book { Id = 4, Title = "Book 4", Price = 8 },
+            new Book { Id = 5, Title = "Book 5", Price = 8 }
+        };
+        }
+
         public IEnumerable<Book> GetAllBooks()
         {
-            throw new NotImplementedException();
+            return _books;
         }
     }
 }
