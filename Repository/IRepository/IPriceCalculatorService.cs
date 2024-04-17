83de7ed86409bfcefd94e420c4f81293a46b7301
Rama Ponna <rama.ponna@neuven.co.uk>
2024-04-17 16:31:14 +0100

    Added IPriceCalculatorService interface

---
+++ Repository/IRepository/IPriceCalculatorService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotterBookStore.Repository.IRepository
+{
+    public interface IPriceCalculatorService
+    {
+    }
+}
---
+++ Repository/PriceCalculatorService.cs
@@ -0,0 +1,13 @@
+﻿using PotterBookStore.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotterBookStore.Repository
+{
+    public class PriceCalculatorService : IPriceCalculatorService
+    {
+    }
+}
