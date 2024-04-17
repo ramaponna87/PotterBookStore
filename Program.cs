ad952c14fbb7a4b30dae4aab3c41941010816341
Rama Ponna <rama.ponna@neuven.co.uk>
2024-04-17 16:53:41 +0100

    Initialized repository and service in the Main method.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
-﻿class Program
+﻿using PotterBookStore.Repository.IRepository;
+using PotterBookStore.Repository;
+
+class Program
 {
     static void Main(string[] args)
     {
+        // Initialize repository and service
+        IBookRepository bookRepository = new BookRepository();
+        IPriceCalculatorService priceCalculatorService = new PriceCalculatorService(bookRepository);
+
         Console.WriteLine("Hello!!");
     }
 }
