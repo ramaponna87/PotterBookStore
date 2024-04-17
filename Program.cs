9e0f3fa246f631ca5b9a6ede40e013f46cb4f8d0
Rama Ponna <rama.ponna@neuven.co.uk>
2024-04-17 16:56:16 +0100

    Created and added books to the basket in the Main method.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using PotterBookStore.Repository.IRepository;
 using PotterBookStore.Repository;
+using PotterBookStore;
 
 class Program
 {
@@ -8,6 +9,15 @@ class Program
         // Initialize repository and service
         IBookRepository bookRepository = new BookRepository();
         IPriceCalculatorService priceCalculatorService = new PriceCalculatorService(bookRepository);
+
+        // Create books
+        var book1 = new Book { Id = 1, Title = "Book 1", Price = 8.0 };
+        var book2 = new Book { Id = 2, Title = "Book 2", Price = 8.0 };
+        var book3 = new Book { Id = 3, Title = "Book 3", Price = 8.0 };
+        var book4 = new Book { Id = 4, Title = "Book 4", Price = 8.0 };
+
+        // Add books to the basket
+        var basket = new List<Book> { book1, book2, book3, book4 };
 
         Console.WriteLine("Hello!!");
     }
